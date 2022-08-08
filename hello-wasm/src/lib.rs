071a8e29d7c9a6537b778077a3d79953919ffbe0
lemonteaa <lemontea.Tom@gmail.com>
2022-08-08 22:20:05 +0000

    Add simple function in wasm

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -17,3 +17,8 @@ extern {
 pub fn greet() {
     alert("Hello, hello-wasm!");
 }
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
---
+++ worker-test/wasm/hello_wasm_bg.js
@@ -0,0 +1,40 @@
+import * as wasm from './hello_wasm_bg.wasm';
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachedUint8Memory0 = new Uint8Array();
+
+function getUint8Memory0() {
+    if (cachedUint8Memory0.byteLength === 0) {
+        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+*/
+export function greet() {
+    wasm.greet();
+}
+
+/**
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+export function add(a, b) {
+    const ret = wasm.add(a, b);
+    return ret;
+}
+
+export function __wbg_alert_a5a2f68cc09adc6e(arg0, arg1) {
+    alert(getStringFromWasm0(arg0, arg1));
+};
+
