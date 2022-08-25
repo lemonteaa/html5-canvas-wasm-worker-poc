61cb258a150a8f5d8d8aec27cb8a80be625856af
lemontea <lemontea.Tom@gmail.com>
2022-08-25 17:49:29 +0000

    test passing buffer web<->wasm

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -18,6 +18,37 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 //    alert("Hello, hello-wasm!");
 //}
 
+
+/// Allocate memory into the module's linear memory
+/// and return the offset to the start of the block.
+#[no_mangle]
+#[wasm_bindgen]
+pub fn my_alloc(len: usize) -> *mut u8 {
+    // create a new mutable buffer with capacity `len`
+    let mut buf = Vec::with_capacity(len);
+    // take a mutable pointer to the buffer
+    let ptr = buf.as_mut_ptr();
+    // take ownership of the memory block and
+    // ensure that its destructor is not
+    // called when the object goes out of scope
+    // at the end of the function
+    std::mem::forget(buf);
+    // return the pointer so the runtime
+    // can write data at this offset
+    return ptr;
+}
+
+/// Given a pointer to the start of a byte array and
+/// its length, return the sum of its elements.
+#[no_mangle]
+pub unsafe fn array_sum(ptr: *mut u8, len: usize) -> u8 {
+    // create a Vec<u8> from the pointer to the
+    // linear memory and the length
+    let data = Vec::from_raw_parts(ptr, len, len);
+    // actually compute the sum and return it
+    data.iter().sum()
+}
+
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     return a + b;
--- worker-test/index.js
+++ worker-test/index.js
@@ -7,12 +7,27 @@ wasmWorker('http://dev-machine:35668/wasm/hello_wasm_bg.wasm', {
     },
   })
 })
-  .then(module => {
-    return module.exports.add(1, 2);
-    //console.log(module.exports.add(1, 2));
-  })
+  .then(wasmModule =>
+    wasmModule.run(({
+      module,
+      instance,
+      importObject,
+      params,
+    }) => {
+      const err = new Error();
+      if (params === undefined) throw err;
+      if (!(module instanceof WebAssembly.Module)) throw err;
+      if (!(instance instanceof WebAssembly.Instance)) throw err;
+      if (importObject.imports === undefined) throw err;
+
+      var ptr = instance.exports.my_alloc(5);
+      var mem = new Uint8Array(instance.exports.memory.buffer, ptr, 5);
+      mem.set(new Uint8Array(params[0]));
+      var res = instance.exports.array_sum(ptr, 5);
+      return res;
+    }, [[1, 2, 3, 4, 5]]))
   .then(sum => {
-    console.log('1 + 2 = ' + sum);
+    console.log('Array sum is = ' + sum);
   })
   .catch(ex => {
     // ex is a string that represents the exception
@@ -37,4 +52,3 @@ wasmWorker('http://dev-machine:35668/wasm/hello_wasm_bg.wasm', {
   .then(result => {
     console.log(result);
   });*/
-
